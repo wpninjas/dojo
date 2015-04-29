c90d07ca945b0830f528ae278e4a422b67a07a46
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2015-04-29 16:45:16 -0400

    Update wordpress-install.sh

--- lib/bash/wordpress-install.sh
+++ lib/bash/wordpress-install.sh
@@ -1,5 +1,5 @@
-chmod -R 777 /var/www/public
-chown -R www-data:www-data /var/www/public
+chmod -R 777 /public/wp
+chown -R www-data:www-data /public/wp
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 php wp-cli.phar core install --url="wp" --title="DOJO Dev" --admin_user="admin" --admin_password="password" --admin_email="test@test.test" --allow-root
