81529377fd771b96d43f76b5d7964dcc8b1137c0
Kyle B. Johnson <kyle@wpninjas.com>
2015-04-29 16:30:42 -0400

    remove virtualbox and vagrant scripts

--- lib/bash/vagrant.sh
+++
@@ -1,6 +0,0 @@
-# http://vaibhavbajpai.com/blog/2012/12/05/installing-vagrant-on-a-headless-debian-server/
-
-sudo apt-get install ruby ruby-dev rubygems
-sudo gem install vagrant
-
-vagrant plugin install vagrant-hostsupdater
--- lib/bash/wordpress.sh
+++ lib/bash/wordpress.sh
@@ -1,22 +1,18 @@
-cd ~/../../var/www
-
-# Permissions
+# WP CLI
 chmod -R 777 .
 chown -R www-data:www-data .
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
 
-# WordPress Directory
+# Download Wordpress
 if [ ! -d public/wp ]
 then
-    # Download WordPress
     git clone https://github.com/WordPress/WordPress.git public/wp
 fi
 
 # Copy over wp-config
 cp lib/config/wp-config.php public/wp/wp-config.php
 
-# WP CLI
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-
 # Install
+cd public/wp
 php wp-cli.phar core install --url="wp" --title="DOJO Dev" --admin_user="admin" --admin_password="password" --admin_email="test@test.test" --allow-root
