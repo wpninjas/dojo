9ff8730837476d65b05775f420f38e2d5c72a8e0
Kyle B. Johnson <kyle@wpninjas.com>
2015-05-08 09:21:13 -0400

    update default password

--- lib/support/bootstrap.sh
+++ lib/support/bootstrap.sh
@@ -4,12 +4,12 @@ chmod -R 777 /var/www/public/wp
 chown -R www-data:www-data /var/www/public/wp
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-php wp-cli.phar core install --url="dojo/wp" --title="WordPress Dev Env" --admin_user="admin" --admin_password="1111" --admin_email="test@test.com" --allow-root
+php wp-cli.phar core install --url="dojo/wp" --title="WordPress Dev Env" --admin_user="admin" --admin_password="password" --admin_email="test@test.com" --allow-root
 echo "|============================|"
 echo "| Installation is complete.  |"
 echo "| Type 'wp/' in your browser |"
 echo "| to access your new site.   |"
 echo "|     Username:Password      |"
-echo "|        admin:1111          |"
+echo "|       admin:password       |"
 echo "|============================|"
 exit
