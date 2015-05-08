50f19475594dd034a779c93e720ee353c3987b3c
Kyle B. Johnson <kyle@wpninjas.com>
2015-05-08 11:25:35 -0400

    add dynamic urls and theme keys and salts

--- lib/config/wp-config.php
+++ lib/config/wp-config.php
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'r78f0B&V)YbjpB%^O=h}2}Z}<JoYTe[!{Rs.{?E_3@m)1F-V!6tr4x,0F_eI3_L0');
-define('SECURE_AUTH_KEY',  '99g-W;v,?@@tD|-%ydB60vLLg(Wa?X)ZcbKElf4Wz/sEGk2uv<OYX=/e~:BMoe;z');
-define('LOGGED_IN_KEY',    '9T-gU_T,JrNUi>>J+7eD7<#gy+u|Bmuf9F}U}w+!7/e3wInq1[k4I~|Ak=]Yn0cS');
-define('NONCE_KEY',        'J3JyXYTZ~Jy.y`!V~s ~-0J<~P[_E`c)V/fS=,:<31cy1C910?G>nI7E#LBu@wXR');
-define('AUTH_SALT',        'ddf00^-XnM/g=uJExwg7Bp]<Yi0Py5(*4HJu{#zr9NdM<jTwPKaPy6?<we4c,j06');
-define('SECURE_AUTH_SALT', 'c53c=;NU5KI2p5!p0 4?p|0B:VxT#*sc+%zF.N6``6@Fb^i&{U)67zP>0ZbhJPve');
-define('LOGGED_IN_SALT',   'QNZ:nD0nWHU[vmBL[Xyga~Xp sL=-RpbE5(SacR^~A[5kpXOGrjp]wW<;5/skB!F');
-define('NONCE_SALT',       '5#UZ_^akrM1c6HQ]*X]H2y<t$p= >>u)XHuJW.4%Gq}-s`S;bAR&aVo:R5p6Lm3n');
+define('AUTH_KEY',         'go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go!go!go!go!');
+define('SECURE_AUTH_KEY',  'go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go!go!go!go!');
+define('LOGGED_IN_KEY',    'go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go!go!go!go!');
+define('NONCE_KEY',        'go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go!go!go!go!');
+define('AUTH_SALT',        'go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go!go!go!go!');
+define('SECURE_AUTH_SALT', 'go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go!go!go!go!');
+define('LOGGED_IN_SALT',   'go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go!go!go!go!');
+define('NONCE_SALT',       'go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go-ninja-go-ninja-go!go!go!go!go!');
 
 /**#@-*/
 
@@ -68,10 +68,14 @@ $table_prefix  = 'wp_';
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
+
+define('WP_HOME','http://' . $_SERVER['SERVER_NAME'] . '/wp');
+define('WP_SITEURL','http://' . $_SERVER['SERVER_NAME'] . '/wp');
 
 define('WP_PLUGIN_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content/plugins' );
 define('WP_PLUGIN_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content/plugins' );
+define('UPLOADS', '../wp-content/uploads' );
 define('WPMU_PLUGIN_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content/mu-plugins');
 
 /* That's all, stop editing! Happy blogging. */
