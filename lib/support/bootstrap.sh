ff00214b6100d674a569577adda3ee923e0bd065
Kyle B. Johnson <kyle@wpninjas.com>
2015-05-08 08:59:32 -0400

    update wordpress location for wp-cli

--- lib/support/bootstrap.sh
+++ lib/support/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-cd /var/www/public
-chmod -R 777 /var/www/public
-chown -R www-data:www-data /var/www/public
+cd /var/www/public/wp
+chmod -R 777 /var/www/public/wp
+chown -R www-data:www-data /var/www/public/wp
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 php wp-cli.phar core install --url="wp" --title="WordPress Dev Env" --admin_user="admin" --admin_password="1111" --admin_email="test@test.com" --allow-root
