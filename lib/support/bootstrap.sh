4da56a33830f5ca6a7de1ca320337128d7b898c6
Kyle B. Johnson <kyle@wpninjas.com>
2015-05-08 09:03:10 -0400

    update url for wp-cli

--- lib/support/bootstrap.sh
+++ lib/support/bootstrap.sh
@@ -4,7 +4,7 @@ chmod -R 777 /var/www/public/wp
 chown -R www-data:www-data /var/www/public/wp
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-php wp-cli.phar core install --url="wp" --title="WordPress Dev Env" --admin_user="admin" --admin_password="1111" --admin_email="test@test.com" --allow-root
+php wp-cli.phar core install --url="dojo/wp" --title="WordPress Dev Env" --admin_user="admin" --admin_password="1111" --admin_email="test@test.com" --allow-root
 echo "|============================|"
 echo "| Installation is complete.  |"
 echo "| Type 'wp/' in your browser |"
